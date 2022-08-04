bf68c259554495dd0d3516fc428362ba2bfa52ae
JR132662 <cooljon08@gmail.com>
2022-08-04 22:14:35 +0000

    saving

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let suits = ["♦", "♥", "♠", "♣"];
+  let Randnum = Math.floor(Math.random() * numbers.length);
+  document.querySelector(".card-number").innerText = numbers[Randnum];
+  console.log(Randnum);
+  let SuitsRdm = Math.floor(Math.random() * suits.length);
+  document.querySelector(".top-suit").innerText = suits[SuitsRdm];
+  document.querySelector(".bottom-suit").innerText = suits[SuitsRdm];
+  if (suits[SuitsRdm] == "♥" || [SuitsRdm] == "♣") {
+    document.querySelector(".top-suit").classList.add("red");
+    document.querySelector(".bottom-suit").classList.add("red");
+    document.querySelector(".card-number").classList.add("red");
+  }
 };
